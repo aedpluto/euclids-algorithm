a49cdeb0d678bf2ba5170bbd737e58d423f057ea
aedpluto <34010125+aedpluto@users.noreply.github.com>
2020-03-04 16:03:57 +0000

    Add files via upload

---
+++ euclid.py
@@ -0,0 +1,17 @@
+"""
+Spyder Editor
+
+Euclid's algorithm
+"""
+import time
+
+def euclid(num1, num2, displayCalculations=False):
+    numbers = [num1, num2]
+    while numbers[-1] != 0:
+        numbers.append(numbers[-2] % numbers[-1])
+        if displayCalculations:
+            time.sleep(1)
+            print(numbers)
+    return numbers[-2]
+
+print(euclid(90, 16))
